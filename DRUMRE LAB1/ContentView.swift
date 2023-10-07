516f6c67fece889219b26e7e0d3b7deb55777a0e
Andre Flego <ade.flego@gmail.com>
2023-10-07 14:32:34 +0200

    Initial Commit

---
+++ DRUMRE LAB1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DRUMRE LAB1
+//
+//  Created by Andre Flego on 07.10.2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DRUMRE LAB1/DRUMRE_LAB1App.swift
@@ -0,0 +1,17 @@
+//
+//  DRUMRE_LAB1App.swift
+//  DRUMRE LAB1
+//
+//  Created by Andre Flego on 07.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct DRUMRE_LAB1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
